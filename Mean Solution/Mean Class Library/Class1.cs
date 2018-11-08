7ea75b2d138351b93bd35080ffeef62b6e3b3b01
Nisharu <39903426+Nisharu@users.noreply.github.com>
2018-11-08 19:45:41 +0200

    Mean

---
+++ Mean Solution/Mean Class Library/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Mean_Class_Library
+
+{
+    class Statistics
+    {
+        double mean;
+        double[] data_array;
+        int m;
+        public double Mean(IEnumerable<double> data)
+        {
+            foreach (double element in data)
+            {
+                if (element == double.NaN)
+                {
+                    return double.NaN;
+                }
+            }
+
+            data_array = data as double[];
+            for (int i = 0; i < data_array.Length; i++)
+            {
+                mean += (data_array[i] - mean) / ++m;
+            }
+            return mean;
+        }
+    }
+
+}
